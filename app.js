c682204cce324d548e62ed2001f3b69418e20a40
kurosiroke <129666467+kurosiroke@users.noreply.github.com>
2023-10-23 12:13:53 +0000

    条件分岐

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-let str1 = 'Hello';
-let str2 = 'World!!';
-alert(str1 + str2);
+let orange = 120;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い');
+}
 
 $(function(){
   $('button').on('click', function(){
