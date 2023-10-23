0b5bbcc3495a95a010e9c38326593d2d498330d1
kurosiroke <129666467+kurosiroke@users.noreply.github.com>
2023-10-23 12:08:54 +0000

    文字列を結合

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-alert('Hello World');
+let str1 = 'Hello';
+let str2 = 'World!!';
+alert(str1 + str2);
+
 $(function(){
   $('button').on('click', function(){
     $('ul').children().css('color','red');
